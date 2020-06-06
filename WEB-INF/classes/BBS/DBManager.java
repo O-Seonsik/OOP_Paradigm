b6cd8b9ccfc19fc6e8d069071ac4570f8d36add6
O-Seonsik <dhtjstlr777@gmail.com>
2020-06-07 04:29:34 +0900

    Modify DBManager

--- WEB-INF/classes/BBS/DBManager.java
+++ WEB-INF/classes/BBS/DBManager.java
@@ -23,15 +23,15 @@ public class DBManager {
         }
     }
 
-    // 데이터베이스 값 조회 - return 필요
-    public ResultSet executeQuery(String SQL) throws ServletException {
+    // 데이터베이스 입력값 유효성 검사
+    public boolean executeQuery(String key, String value, String table) throws ServletException {
         DBConnect dbConnect = new DBConnect();
         Connection conn = dbConnect.getConn();
         Statement stmt = dbConnect.getStmt();
-        ResultSet rs = null;
         try{
-            rs = stmt.executeQuery(SQL);
-            return rs;
+            ResultSet rs = stmt.executeQuery("SELECT * FROM " + table + " WHERE " + key +  " = '" + value + "'");
+            if(rs.next()) return true;
+            else return false;
         }catch (Exception e){
             throw new ServletException();
         }finally{
